b0619c73a2fb4db0e158dafd987fe11eea65513d
Milan <milanpanda4425@gmail.com>
2023-07-13 13:21:07 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -16,24 +16,49 @@
  -> To enable HTTPS on dev build   npx parcel index.html --https
  -> Consistent Hashing Algorithms
  -> Zero Configuration
+ -> Tree Shaking - Removing unwanted code
 
 */
-
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {
-  id: "heading1"
-}, "Heading 1");
-const heading2 = React.createElement("h2", {
-  id: "heading2"
-}, "Heading 2");
+// const heading1 = React.createElement("h1", {
+//   id: "heading1",
+//   key: "h1"
+// }, "Heading 1");
+// //React.createElement => Object => HTML(DOM)
+// console.log(heading1);
 
-const container = React.createElement("div", {
-  id: "container"
-}, [heading1, heading2]);
+// const heading2 = React.createElement("h2", {
+//   id: "heading2",
+//   key: "h2"
+// }, "Heading 2");
+
+// const container = React.createElement("div", {
+//   id: "container"
+// }, [heading1, heading2]);
+
+//JSX
+const title = (
+  <h1 id="h3" key="h3">
+    Hello Folks!!!
+  </h1>
+);
+
+// React Component
+// - Functional Component
+const HeadingComponent = () => {
+  return (
+    <div>
+      {title}
+      <h1>This is H1</h1>
+      <h2>This is H2</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+// root.render(heading3);
+root.render(<HeadingComponent/>);
